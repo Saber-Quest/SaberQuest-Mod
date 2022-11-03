00750fd1de543ad2d7e2bdfe4fedd67311a3ed14
Sirspam <71392316+Sirspam@users.noreply.github.com>
2022-11-03 16:54:09 +0000

    Setup

---
+++ SaberQuest/Configuration/PluginConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+
+namespace SaberQuest.Configuration
+{
+	internal class PluginConfig
+	{
+		// public virtual int MeaningofLife = 42 { get; set; } 
+	}
+}
---
+++ SaberQuest/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using IPA;
+using IPA.Config;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace SaberQuest
+{
+	[Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
+	public class Plugin
+	{
+		[Init]
+		public void Init(IPALogger logger, Config config, Zenjector zenjector)
+		{
+			zenjector.UseSiraSync();
+			zenjector.UseLogger(logger);
+			zenjector.UseMetadataBinder<Plugin>();
+		}
+	}
+}
